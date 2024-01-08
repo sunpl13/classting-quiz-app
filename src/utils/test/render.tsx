b5d4e91dfc9a305e741aa7f68788eb917f3d3595
jungsoo <sunpl13@naver.com>
2024-01-08 19:30:04 +0900

    initial commit

---
+++ src/utils/test/render.tsx
@@ -0,0 +1,12 @@
+import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import React from "react";
+
+export default async (component: React.ReactElement) => {
+  const user = userEvent.setup();
+
+  return {
+    user,
+    ...render(component),
+  };
+};
