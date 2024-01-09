a16cde0efc43220c2c0b1b0494c60e9158e4d77b
jungsoo <sunpl13@naver.com>
2024-01-09 16:59:49 +0900

    chore: 퀴즈 response 타입 설정

---
+++ src/types/quiztype.ts
@@ -0,0 +1,19 @@
+type Difficulty = "any" | "easy" | "medium" | "hard";
+type QuizTypeSelect = "any" | "multiple" | "boolean";
+type Category = number | "any";
+
+export type QuizParams = {
+  amount: number;
+  category: Category;
+  difficulty: Difficulty;
+  type: QuizTypeSelect;
+};
+
+export type TriviaQuizResponse = {
+  type: QuizTypeSelect;
+  difficulty: Difficulty;
+  category: string;
+  question: string;
+  correct_answer: string;
+  incorrect_answers: string[];
+};
