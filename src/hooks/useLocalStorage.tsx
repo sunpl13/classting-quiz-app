58770f36dafcc0ce2fc0b718a1c910c3e51138ac
jungsoo <sunpl13@naver.com>
2024-01-09 22:37:45 +0900

    feat: 로컬 스토리지를 관리할 수 있는 hook

---
+++ src/hooks/useLocalStorage.tsx
@@ -0,0 +1,30 @@
+const useLocalStorage = (key: string) => {
+  const setItem = (value: unknown) => {
+    try {
+      window.localStorage.setItem(key, JSON.stringify(value));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getItem = () => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : undefined;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const removeItem = () => {
+    try {
+      window.localStorage.removeItem(key);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return { getItem, setItem, removeItem };
+};
+
+export default useLocalStorage;
