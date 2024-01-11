eca3562028df11fc26825b9aec594b8410c8907f
jungsoo <sunpl13@naver.com>
2024-01-11 19:58:31 +0900

    feat: 로딩 스켈레톤 UI 추가

--- src/components/QuizWrapper.tsx
+++ src/components/QuizWrapper.tsx
@@ -1,22 +1,23 @@
-import React from 'react';
 import { useLocation } from 'react-router-dom';
 import useGetQuiz from '../hooks/useGetQuiz';
 import Quiz from './Quiz';
+import LoadingSkeleton from './skeletons/LoadingSkeleton';
+import Error from './Error';
 
 const QuizWrapper = () => {
   const location = useLocation();
+
   const quizInfo = { ...location.state };
   const { isLoading, data } = useGetQuiz(quizInfo);
   console.log(data, isLoading);
 
-  if (isLoading) {
-    return <div>Loading...</div>;
+  if (isLoading || data === null) {
+    return <LoadingSkeleton />;
   }
 
   if (data.code === 'error') {
-    return <div>error!</div>;
+    return <Error />;
   }
-
   return <Quiz questions={data.data.results} />;
 };
 
---
+++ src/components/skeletons/LoadingSkeleton.tsx
@@ -0,0 +1,42 @@
+import styled from '@emotion/styled';
+import Skeleton from './Skeleton';
+
+const LoadingSkeleton = () => {
+  return (
+    <Wrapper>
+      <Skeleton width={40} height={30} $mb={10} />
+      <Skeleton width={380} height={45} $mb={20} />
+
+      {Array.from({ length: 4 }).map((_, idx) => {
+        return (
+          <Skeleton
+            key={idx}
+            width={100}
+            height={48}
+            $wUnit={'%'}
+            $rounded={true}
+            $mb={10}
+          />
+        );
+      })}
+      <div className="flx-end">
+        <Skeleton width={120} height={53} />
+      </div>
+    </Wrapper>
+  );
+};
+
+export default LoadingSkeleton;
+
+const Wrapper = styled.div`
+  width: 500px;
+  background: var(--bg);
+  border-radius: 4px;
+  margin-top: 100px;
+  padding: 30px 60px;
+
+  & .flx-end {
+    display: flex;
+    justify-content: flex-end;
+  }
+`;
